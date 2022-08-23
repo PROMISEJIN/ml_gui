2074a26a49d6280b455ec92c70ed2dd124f6a33d
PROMISEJIN <105270902+PROMISEJIN@users.noreply.github.com>
2022-08-23 14:11:37 +0900

    First Commit

--- data_visualize.py
+++ data_visualize.py
@@ -13,7 +13,7 @@ class data_:
     def get_column_list(self , df):
         column_list = []
         
-        for i in df.columns :
+        for i in df.columns:
             column_list.append(i)
             
         return column_list
@@ -30,7 +30,7 @@ class data_:
         scaled_features_df = pd.DataFrame(scaled_features, index=x.index , columns= x.columns)
         scaled_features_df[target] = df[target]
         
-        return scaled_features
+        return scaled_features_df
     
     def MinMaxScale(self, df, target):
         sc = MinMaxScaler()
@@ -40,7 +40,7 @@ class data_:
         scaled_features_df = pd.DataFrame(scaled_features, index=x.index , columns= x.columns)
         scaled_features_df[target] = df[target]
         
-        return scaled_features
+        return scaled_features_df
     
     def PowerScale(self, df, target):
         sc = PowerTransformer()
@@ -50,8 +50,92 @@ class data_:
         scaled_features_df = pd.DataFrame(scaled_features, index=x.index , columns= x.columns)
         scaled_features_df[target] = df[target]
         
-        return scaled_features
+        return scaled_features_df
     
-    
+    def convert_category(self, df , column):
+        le = LabelEncoder()
+        df[column] = le.fit_transform(df[column])
+        return df[column]
         
     
+    def fillmean(self, df , column):
+        df[column].fillna(df[column].mean(), inplace = True)
+        return df[column]
+        
+        
+    def fillna(self, df, column):
+        df[column].fillna(0, inplace = True)
+        return df[column]
+        
+        
+    def scatter_plot(self,df,x,y,c,marker):
+        plt.figure()
+        plt.scatter(df[x],df[y],marker=marker,c=c)
+        plt.xlabel(x)
+        plt.ylabel(y)
+        plt.title(y + " vs "+ x)
+        plt.show()
+        
+    def line_plot(self,df,x,y,c,marker):
+        plt.figure()
+        plt.plot(df[x],df[y],marker=marker,c=c)
+        plt.xlabel(x)
+        plt.ylabel(y)
+        plt.title(y + " vs "+ x)
+        plt.show()
+            
+            
+            
+            
+            
+# from ast import Pass
+# import atexit
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import numpy as np 
+# from sklearn.preprocessing import LabelEncoder , StandardScaler ,MinMaxScaler , PowerTransformer
+
+
+# class data_:
+    
+#     def read_file(self, filepath):
+#         return pd.read_csv(str(filepath))
+    
+#     def get_column_list(self , df):
+#         column_list = []
+        
+#         for i in df.columns:
+#             column_list.append(i)
+        
+#         return column_list
+    
+#     def drop_columns(self , df ,column):
+#         return df.drop(column, axis=1)
+        
+#     def StandardScale(self, df , target):
+#         sc = StandardScaler()
+#         x = df.drop(target, axis=1)
+#         scaled_features = sc.fit_transform(x)
+#         scaled_features_df = pd.DataFrame(scaled_features , index=x.index , columns=x.columns)
+#         scaled_features_df[target] = df[target]
+        
+#         return scaled_features_df 
+    
+#     def MinMaxScale(self, df , target):
+#         sc = MinMaxScaler()
+#         x = df.drop(target, axis=1)
+#         scaled_features = sc.fit_transform(x)
+#         scaled_features_df = pd.DataFrame(scaled_features , index=x.index , columns=x.columns)
+#         scaled_features_df[target] = df[target]
+        
+#         return scaled_features_df
+    
+#     def PowerScale(self, df , target):
+#         sc = PowerTransformer()
+#         x = df.drop(target, axis=1)
+#         scaled_features = sc.fit_transform(x)
+#         scaled_features_df = pd.DataFrame(scaled_features , index=x.index , columns=x.columns)
+#         scaled_features_df[target] = df[target]
+        
+#         return scaled_features_df    
--- ml_gui_program.py
+++ ml_gui_program.py
@@ -3,6 +3,8 @@ import sys, pickle
 from PyQt5 import uic, QtWidgets
 from data_visualize import data_
 from table_display import DataFrameModel
+import linear_reg , logistic_reg, RandomForest, add_steps, mlp
+
 
 class UI(QMainWindow):
     
@@ -12,6 +14,7 @@ class UI(QMainWindow):
         
         global data, steps
         data = data_()
+        steps = add_steps.add_steps()
         
         self.Browse = self.findChild(QPushButton, "Browse") # findChild는 내가 쓰고 싶은 버튼을 찾아주는 함수
         self.columns = self.findChild(QListWidget, "Column_list")
@@ -25,17 +28,44 @@ class UI(QMainWindow):
         self.Scaler= self.findChild(QComboBox, "Scaler") # Scale 하는 함수 호출 
         self.Scale_btn= self.findChild(QPushButton, "Scale_btn") # Scale 오른쪽 버튼
         
+        self.categori_column= self.findChild(QComboBox, "categori_column") #
+        self.convert_btn= self.findChild(QPushButton, "convert_btn") #
         
+        self.Fill_Mean= self.findChild(QPushButton, "Fill_Mean") #
+        self.Fill_unknown= self.findChild(QPushButton, "Fill_unknown") #
+        self.Empty_Column= self.findChild(QComboBox, "Empty_Column") #
         
-        self.Browse.clicked.connect(self.get_csv) ## Browse를 클릭했을 때 실행되게 만드는 함수 clicked.connect()
+        self.scatter_x= self.findChild(QComboBox, "scatter_x") # scatter 버튼과 combobox 등록 
+        self.scatter_y= self.findChild(QComboBox, "scatter_y") #
+        self.scatter_marker= self.findChild(QComboBox, "scatter_marker") #
+        self.scatter_c = self.findChild(QComboBox, "scatter_c") #
+        self.scatter_btn= self.findChild(QPushButton, "scatter_btn") #
+        
+        self.line_x= self.findChild(QComboBox, "line_x") # line 버튼과 combobox 등록 
+        self.line_y= self.findChild(QComboBox, "line_y") #
+        self.line_marker= self.findChild(QComboBox, "line_marker") #
+        self.line_c = self.findChild(QComboBox, "line_c") #
+        self.line_btn= self.findChild(QPushButton, "line_btn") #
+        
+        self.model_select = self.findChild(QComboBox, "model_select") #
+        self.train= self.findChild(QPushButton, "train") #
+        
+        self.Browse.clicked.connect(self.get_csv) ## Browse를 클릭했을 때 실행되게 만드는 함수 clicked.connect(), get_csv는 밑에서 함수를 생성 
         self.columns.clicked.connect(self.target)
         self.Submit.clicked.connect(self.set_target)
         self.drop.clicked.connect(self.dropc)
         
         self.Scale_btn.clicked.connect(self.scale_value)
+        self.convert_btn.clicked.connect(self.convert_cat)
         
+        self.Fill_Mean.clicked.connect(self.fillme)
+        self.Fill_unknown.clicked.connect(self.fillna)
         
+        self.scatter_btn.clicked.connect(self.scatter_plt)
         
+        self.line_btn.clicked.connect(self.line_plt)
+        
+        self.train.clicked.connect(self.train_func)
         
         # self.show() 
     def filldetails(self, fleg = 1):
@@ -45,6 +75,7 @@ class UI(QMainWindow):
         self.columns.clear() # columns 안에 있는 내용들을 지워버리는 함수
         self.column_list = data.get_column_list(self.df)
         # print(self.column_list)    
+        
         
         for i , j in enumerate(self.column_list): ## enumerate() 함수는 인덱스까지 뽑게 만들어 줄 수 있음.
             stri = f'{j} ------ {str(self.df[j].dtype)}' ##j는 리스트 이름, 데이터 타입 보기
@@ -72,7 +103,7 @@ class UI(QMainWindow):
         if self.Scaler.currentText() == 'StandardScale':
             self.df = data.StandardScale(self.df, self.target_value)
             
-        elif self.Scaler.currentText() == 'MinMaxScaler':
+        elif self.Scaler.currentText() == 'MinMaxScale':
             self.df = data.MinMaxScale(self.df, self.target_value)
         
         else :
@@ -81,14 +112,70 @@ class UI(QMainWindow):
         self.filldetails()
         
     def fill_combo_box(self):
+        
         self.dropcolumn.clear()
         self.dropcolumn.addItems(self.column_list)
+        
+        self.categori_column.clear()
+        self.categori_column.addItems(self.column_list)
+        
+        self.Empty_Column.clear()
+        self.Empty_Column.addItems(self.column_list)
+        
+        self.scatter_x.clear()
+        self.scatter_x.addItems(self.column_list)
+        
+        self.scatter_y.clear()
+        self.scatter_y.addItems(self.column_list)
+        
+        self.line_x.clear()
+        self.line_x.addItems(self.column_list)
+        
+        self.line_y.clear()
+        self.line_y.addItems(self.column_list)
+        
         x = DataFrameModel(self.df)
         self.table.setModel(x)
         
-        
+    def convert_cat(self):
+        selected = self.categori_column.currentText()
+        self.df[selected] = data.convert_category(self.df , selected)
+        self.filldetails()
 
+    def fillme(self):
+        selected = self.Empty_Column.currentText()
+        type = self.df[selected].dtype
+        
+        if type != 'object':
+            self.df[selected] = data.fillmean(self.df , selected)
+            self.filldetails()
+        else : 
+            print("datatype is object")
             
+    def fillna(self):
+        selected = self.Empty_Column.currentText()
+        self.df[selected] = data.fillna(self.df, selected)
+        self.filldetails()
+    
+    def scatter_plt(self):
+        x = self.scatter_x.currentText()
+        y = self.scatter_y.currentText()
+        c = self.scatter_c.currentText()
+        marker = self.scatter_marker.currentText()
+        data.scatter_plot(df = self.df , x = x, y=y, c=c,marker = marker)
+    
+    def line_plt(self):
+        x = self.line_x.currentText()
+        y = self.line_y.currentText()
+        c = self.line_c.currentText()
+        marker = self.line_marker.currentText()
+        data.line_plot(df = self.df , x = x, y=y, c=c,marker = marker)
+    
+    def train_func(self):
+        myModel = {"LinearRegression" : linear_reg , "RandomForest" : RandomForest, "MLP" : mlp , "LogisticRegression" : logistic_reg}
+        selected = self.model_select.currentText()
+        self.win = myModel[selected].UI(self.df, self.target_value, steps)
+    
     
     def get_csv(self):
         self.file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "", "csv(*.csv)")
@@ -96,6 +183,7 @@ class UI(QMainWindow):
         
         if self.file_path != "":
             self.filldetails(0)
+        
         
         
 if __name__ == '__main__' :
